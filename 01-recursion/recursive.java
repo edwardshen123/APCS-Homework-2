550367b2dd63e80a7a1f03e304b860e62ea2b618
edwardshen123 <edwardshen123@gmail.com>
2015-02-06 11:33:59 -0500

    hw 1

--- 01-recursion/recursive.java
+++ 01-recursion/recursive.java
@@ -2,6 +2,11 @@ public class recursive {
 
     public char[] cArray;
     
+    public static int factorial(int n) {
+	if (n <= 1) return 1;
+	return n * factorial(n - 1);
+    }
+
     public static int fib(int n) {
 	if (n == 0) {
 	    return 0;
@@ -20,6 +25,7 @@ public class recursive {
 	return 1 + len(str.substring(1));
     }
 
+    /*
     //Method 2
     public int len(String str) {
 	cArray = str.toCharArray();
@@ -28,6 +34,7 @@ public class recursive {
 	}
 	return 1 + len(new String(cArray[1
     }
+    */
     
     public static int count(String str, String c) {
 	if (str.equals("")) {
@@ -40,6 +47,7 @@ public class recursive {
     }
     
     public static void main(String[] args) {
+	System.out.println(factorial(5));
 	System.out.println(fib(3));
 	String str = "Hello World";
 	System.out.println(len(str));
