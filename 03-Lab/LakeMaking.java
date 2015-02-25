a29d26bce9971887239f0fe0697d29c2f2a6db14
edwardshen123 <edwardshen123@gmail.com>
2015-02-25 00:47:07 -0500

    finished lake making

--- 03-Lab/LakeMaking.java
+++ 03-Lab/LakeMaking.java
@@ -2,9 +2,12 @@ import java.util.Random;
 
 public class LakeMaking {
 
-    private Random randInt = new Random();
+    //private Random randInt = new Random();
     
-    private int[][] pasture;
+    private int[][] pasture = {{28, 25, 20, 32, 34, 36},
+			       {27, 25, 20, 20, 30, 34},
+			       {24, 20, 20, 20, 20, 30},
+			       {20, 20, 14, 14, 20, 20}};
     private int pastureX;
     private int pastureY;
     
@@ -16,16 +19,21 @@ public class LakeMaking {
 	this.pastureX = pastureX;
 	this.pastureY = pastureY;
 
+	/*
 	pasture = new int[pastureY][pastureX];
+
 	for (int r = 0; r < pastureY; r++) {
 	    for (int c = 0; c < pastureX; c++) {
 		pasture[r][c] = randInt.nextInt(27) + 10;
 	    }
 	}
+	*/
     }
     
     public void stomp(int y, int x, int stomps) {
         int maxVal = 0;
+	y--;
+	x--;
 	for (int r = y; r < y + 3; r++) {
 	    for (int c = x; c < x + 3; c++) {
 		if (pasture[r][c] > maxVal) {
@@ -36,8 +44,10 @@ public class LakeMaking {
 
 	for (int r = y; r < y + 3; r++) {
 	    for (int c = x; c < x + 3; c++) {
-		int stompsRemoved = pasture[r][c] - maxVal;
-		pasture[r][c] -= stomps + stompsRemoved;
+		int stompsRemoved = maxVal - pasture[r][c];
+		if (stompsRemoved < stomps) {
+		    pasture[r][c] -= stomps - stompsRemoved;
+		}
 	    }
 	}
     }
@@ -66,13 +76,18 @@ public class LakeMaking {
 	    for (int c = 0; c < pastureX; c++) {
 		s += String.format("%3d", pasture[r][c]);
 	    }
+	    s += "\n";
 	}
 	return s;
     }
 
     public static void main(String[] args) {
 	LakeMaking lk = new LakeMaking();
+	lk.stomp(1, 4, 4);
+	lk.stomp(1, 1, 10);
 	System.out.println(lk);
+	System.out.println(lk.TDepth(22));
+	System.out.println(lk.volume());
     }
     
 }
