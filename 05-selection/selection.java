6909d3d71ddc7c4ca9623904def4c5f5e4ace914
edwardshen123 <edwardshen123@gmail.com>
2015-03-08 17:31:13 -0400

    finished rebooted selection

--- 05-selection/selection.java
+++ 05-selection/selection.java
@@ -2,27 +2,36 @@ import java.util.Arrays;
 
 public class selection {
 
-    public static int[] select(int[] A, int k, int l, int h) {
+    public static int select(int[] A, int k, int l, int h) {
 	int pivotIndex = l + (h - 1) / 2;
 	int pivot = A[pivotIndex];
+	int pivotHigh = h;
 	int low = l;
 	int high = h;
-	swap(A, A[l+ (h - 1) / 2], A[high]);
+	swap(A, (l + h) / 2, high);
 	high--;
 	while (low < high) {
 	    if (A[low] < pivot) {
 		low++;
 	    } else {
-		swap(A, A[low], A[high]);
+		swap(A, low, high);
 		high--;
 	    }
 	}
 	if (A[low] > pivot) {
-	    swap(A, A[low], A[]);
+	    swap(A, low, pivotHigh);
+	    pivotIndex = low;
 	} else {
-	    swap(A, A[low + 1], A[pivotIndex]);
+	    swap(A, low + 1, pivotHigh);
+	    pivotIndex = low + 1;
 	}
-	return A;
+	if (pivotIndex == k) {
+	    return A[k];
+	} else if (pivotIndex > k) {
+	    return select(A, k, l, pivotIndex - 1);
+	} else {
+	    return select(A, k, pivotIndex + 1, h);
+	}
     }
 
     public static void swap(int[] A, int aIndex, int bIndex) {
@@ -32,7 +41,7 @@ public class selection {
     }
 
     public static void main(String[] args) {
-	int[] test = {3, 34, 6, 2, 1, 6, 43, 8, 23};
-	System.out.println(Arrays.toString(selection.select(test, 0, 0, test.length - 1)));
+	int[] test = {3, 34, 0, 2, 1, 6, 43, 8, 23};
+	System.out.println(select(test, 3, 0, test.length - 1));
     }
 }
