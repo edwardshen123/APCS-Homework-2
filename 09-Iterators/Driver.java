26e9ec3472f4253fb7290758378eac07a6406e31
edwardshen123 <e@eshen.me>
2015-03-20 10:55:01 -0400

    update

--- 09-Iterators/Driver.java
+++ 09-Iterators/Driver.java
@@ -1,30 +1,37 @@
+import java.util.*;
+
 public class Driver{
-		public static void main(String[] args) {
-				LList<String> l = new LList<String>();
+    public static void main(String[] args) {
+	LList<String> l = new LList<String>();
+	LList<Integer> l2 = new LList<Integer>();
+	
+	l.add("Hello");
+	l.add("world");
+	l.add("abc");
+	l.add("something");
+	l.add("else");
+	
+	System.out.println(l);
 
-				l.add("Hello");
-				l.add("world");
-				l.add("abc");
-				l.add("something");
-				l.add("else");
-
-				System.out.println(l);
-
-				LList<Integer> l2 = new LList<Integer>();
-				for (int i=0;i < 5; i++){
-						l2.add(i);
-				}
-
-				System.out.println(l2);
-
-				LLit<String> i = l.getLLit();
-				while (i.hasNext())
-							 System.out.println(i.next());
+	for (int i=0;i < 5; i++){
+	    l2.add(i);
+	}
+	
+	System.out.println(l2);
+	
+	Iterator<String> i = l.iterator();
+	while (i.hasNext())
+	    System.out.println(i.next());
 				
-				LLit<Integer> i2 = l2.getLLit();
-				while (i2.hasNext())
-							 System.out.println(i2.next());
-				
+	Iterator<Integer> i2 = l2.iterator();
+	while (i2.hasNext())
+	    System.out.println(i2.next());
 
-		}
+	//Java for each or for in loops
+	for (String s: l) {
+	    System.out.print(s + ", ");
+	}
+	
+	
+    }
 }
--- 09-Iterators/LLit.java
+++ 09-Iterators/LLit.java
@@ -1,16 +1,24 @@
-public class LLit<E>{
-		private Node<E> t;
-		public LLit(Node<E> n){
-				t=n;
-		}
+import java.util.*;
 
-		public boolean hasNext(){
-				return t!=null;
-		}
+public class LLit<E> implements Iterator<E>{
+    private Node<E> t;
+    private Node<E> p;
+    public LLit(Node<E> n){
+	t=n;
+    }
+    
+    public boolean hasNext(){
+	return t!=null;
+    }
+    
+    public E next(){
+	E retval = t.getData();
+	p = t;
+	t=t.getNext();
+	return retval;
+    }
 
-		public E next(){
-				E retval = t.getData();
-				t=t.getNext();
-				return retval;
-		}
+    public void remove() {
+	
+    }
 }
