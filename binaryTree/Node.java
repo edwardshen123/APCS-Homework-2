ec099a95ee519573ac7f28dbaf34f3dc14e324b4
edwardshen123 <e@eshen.me>
2015-04-24 09:26:57 -0400

    updated

--- binaryTree/Node.java
+++ binaryTree/Node.java
@@ -1,15 +1,18 @@
-public class Node {
+public class Node implements Comparable{
     private int data;
     private Node left, right;
 
     public Node() {
-	Node(0);
+	this(0);
     }
 
     public Node(int data) {
 	this.data = data;
 	left = null;
 	right = null;
+    }
+
+    public int compareTo(int i) {
     }
 
     public int getData() {
--- binaryTree/tree.java
+++ binaryTree/tree.java
@@ -1,14 +1,15 @@
 public class tree{
 
     private Node root;
+    private Node T2;
 
     public tree() {
 	root = null;
+	T2 = null;
     }
 
-    public Node search(integer i) {
+    public Node search(int i) {
 	Node T = root;
-	Node T2;
 	while (T != null) {
 	    T2 = T;
 	    int c = T.getData().compareTo(i);
@@ -19,8 +20,33 @@ public class tree{
 	return T;
     }
 
-    public void insert(integer i) {
+    public void insert(int i) {
 	Node tmp = new Node(i);
+	if (root == null) {
+	    root = tmp;
+	} else {
+	    Node child = search(i);
+	    if (child != null) {
+		int t = i.compareTo(child.getData());
+		if (t > 0) {
+		    tmp.setLeft(child);
+		} else {
+		    tmp.setRight(child);
+		}
+	    }
+	    int t2 = T2.getData().compareTo(i);
+	    if (t > 0) {
+		t.setLeft(tmp);
+	    } else {
+		t.setRight(tmp);
+	    }
+	}
+    }
+
+    public static void main(String[] args) {
+	tree t = new tree();
+	t.insert(1);
+	t.insert(2);
     }
     
 }
